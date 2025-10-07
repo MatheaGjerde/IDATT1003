a452e87e57f9eafbffa4fd54cdd4ada890aa7070
MatheaGjerde <mathea_10_@hotmail.com>
2025-10-07 11:00:16 +0200

    Delete Ovinger directory

--- Ovinger/oving1/SekundBeregning.java
+++
@@ -1,8 +0,0 @@
-package oving1;
-
-public class SekundBeregning {
-    public static void main(String[] args) {
-        Tid t = new Tid(1, 20, 30);
-        System.out.println(t + " er " + t.tilSekunder() + " sekunder");
-    }
-}
