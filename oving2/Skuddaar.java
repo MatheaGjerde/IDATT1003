751a8b60eb9c61bd7e4aef54eed3aeaaef11c5e9
MatheaGjerde <mathea_10_@hotmail.com>
2025-10-28 09:58:26 +0100

    fikset på feilmelding

--- oving2/Skuddaar.java
+++ oving2/Skuddaar.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 
 public class Skuddaar {
     public static void main(String[] args){
-        Scanner In = new Scanner(System.in);
-        int aarstall = In.nextInt();
-        
-        if ((aarstall % 4 == 0 && aarstall % 100 != 0)|| (aarstall % 400 == 0)){
-            System.out.println(aarstall + " er et skuddår");
+        try(Scanner In = new Scanner(System.in)){
+            int aarstall = In.nextInt();
+            
+            if ((aarstall % 4 == 0 && aarstall % 100 != 0)|| (aarstall % 400 == 0)){
+                System.out.println(aarstall + " er et skuddår");
+            }
+            else {
+                System.out.println(aarstall + " er ikke et skuddår");
+            }
         }
-        else {
-            System.out.println(aarstall + " er ikke et skuddår");
-        }
+    
     }
+    
 }
